58e732e3635d0139c64df585d545bf0460ed4a80
fatima assila <fatima.assila.ouchene@gmail.com>
2024-09-19 19:04:21 +0200

    install mongoose /authentification

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 const mongoose = require('mongoose');
+const bookRoutes = require('./routes/book');
+const userRoutes = require('./routes/user');
+
 
 
 mongoose.connect('mongodb+srv://books:123@cluster0.ib5fx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
@@ -19,39 +22,7 @@ app.use((req, res, next) => {
     next();
 });
 
-
-app.post('/api/books', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-      message: 'livre ajouter !'
-    });
-});
-
-app.get('/api/books', (req, res, next) => {
-    const stuff = [
-      {
-        _id: '1',
-        title: 'Mon premier livre',
-        author: 'Les infos de mon premier livre',
-        year:1999,
-        genre:'architecture',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        rating: 4,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: '2',
-        title: 'Mon deuxième livre',
-        author: 'Les infos de mon premier livre',
-        year:2009,
-        genre:'jardinage',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        rating: 3,
-        userId: 'qsomihvqios',
-      },
-    ];
-    res.status(200).json(stuff);
-});
-
+app.use ( '/api/books' , bookRoutes );
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
---
+++ backend/routes/book.js
@@ -0,0 +1,12 @@
+const express=require('express');
+const router = express.Router();
+const bookCtrl = require('../controllers/book');
+
+
+router.post('/', bookCtrl.creatBook);   
+router.put('/:id',bookCtrl.modifyBook );
+router.delete('/:id', bookCtrl.deleteBook); 
+router.get('/:id',bookCtrl.getOneBook );
+router.get('/',bookCtrl.getAllBook );
+
+module.exports = router;
